a0390cc8c7fe7b16c91bff65deeb67acbd0602bb
ZHOU Yi hua <yihua.zhou@groupe-esigelec.org>
2026-03-16 14:59:00 +0100

    User

--- src/main/java/Raspberry/model/User.java
+++ src/main/java/Raspberry/model/User.java
@@ -1,9 +1,73 @@
 package Raspberry.model;
 
 public class User {
+	private int id;
+	private String username;
+	private String rfidTag;
+	private double balance;
+	private boolean active;
+	private String role;
+
 
 	public User() {
-		// TODO Auto-generated constructor stub
+		this.active = true;
+		this.balance = 0.0;
+
 	}
 
+	public User(String username, String rfidTag, double balance,  String role) {
+		this.username = username;
+		this.rfidTag = rfidTag;
+		this.balance = balance;
+		this.active = true;
+		this.role = role;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getRfidTag() {
+		return rfidTag;
+	}
+
+	public void setRfidTag(String rfidTag) {
+		this.rfidTag = rfidTag;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
 }
