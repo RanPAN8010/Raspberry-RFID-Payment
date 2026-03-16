0993c00ed3757e67c61b00cefdcf03d2ba851e95
RanPAN <yuanlan8010@gmail.com>
2026-03-16 15:18:48 +0100

    Save my changes before pull

--- src/main/java/Raspberry/APP.java
+++ src/main/java/Raspberry/APP.java
@@ -1,15 +1,18 @@
 package Raspberry;
 
+import Raspberry.web.SimpleHttpServer;
+
 public class APP {
 	public static void main(String[] args) {
-        System.out.println("RFID Payment System is starting...");
-        // 模拟一个持续运行的服务，防止容器退出
-        while(true) {
-            try {
-                Thread.sleep(10000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+		System.out.println("RFID Payment System is starting...");
+		        
+		        // 启动 Web 服务器
+		        SimpleHttpServer server = new SimpleHttpServer();
+		        server.start(8080); // 这里的端口要和 docker-compose.yml 里的端口对应
+		        
+		        // 保持程序运行
+		        while(true) {
+		            try { Thread.sleep(10000); } catch (InterruptedException e) {}
+		        }
     }
 }
