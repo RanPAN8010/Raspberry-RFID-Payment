352102fc1ea7e16be901605db9351f64c3cde5bb
ZHOU YIHUA <zhouyihua258@gmail.com>
2026-03-30 21:31:44 +0200

    删除部分文本错误并增加蜂鸣器

--- read_rfid.py
+++ read_rfid.py
@@ -2,20 +2,32 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import sys
+import time  #
 
-# 初始化读卡器
+
 reader = SimpleMFRC522()
 
+
+BUZZER_PIN = 12
+
 try:
-    # 阻塞程序，直到有卡片靠近
+
+    GPIO.setup(BUZZER_PIN, GPIO.OUT)
+    GPIO.output(BUZZER_PIN, GPIO.LOW)
+
+
     id, text = reader.read()
-    
-    # 打印读取到的纯数字 ID（Java 代码会抓取这一行打印的内容）
+
+
+    GPIO.output(BUZZER_PIN, GPIO.HIGH)
+    time.sleep(0.15)
+    GPIO.output(BUZZER_PIN, GPIO.LOW)
+
     print(id)
-    sys.stdout.flush()  # 刷新缓冲区，确保 Java 能立刻读到
-    
+    sys.stdout.flush()
+
 except Exception as e:
     print("Error:", e)
 finally:
-    # 释放引脚资源，这步非常重要，否则下次运行会报错
+
     GPIO.cleanup()
